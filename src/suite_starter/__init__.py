e9dffd5954c70ecd80be41790226c79692751006
Tobias Persson <tobiaspn@axis.com>
2026-02-18 09:10:17 +0100

    Ran black

--- src/suite_starter/__init__.py
+++ src/suite_starter/__init__.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """ETOS suite starter module."""
+
 import os
 from importlib.metadata import PackageNotFoundError, version
 
